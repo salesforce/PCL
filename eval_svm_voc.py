af7287d0a46674a3dad6c2a6b2a4fd8e899e252e
root <ljn12260409@gmail.com>
2020-07-15 08:39:36 +0000

    code

---
+++ eval_svm_voc.py
@@ -0,0 +1,244 @@
+from __future__ import print_function
+
+import os
+import sys
+import time
+import torch
+import torch.optim as optim
+import torch.backends.cudnn as cudnn
+import torch.nn.functional as F
+import argparse
+import random
+import numpy as np
+
+from torchvision import transforms, datasets
+import torchvision.models as models
+
+from voc import Voc2007Classification
+
+from sklearn.svm import LinearSVC
+
+
+def parse_option():
+    model_names = sorted(name for name in models.__dict__
+        if name.islower() and not name.startswith("__")
+        and callable(models.__dict__[name]))
+
+    parser = argparse.ArgumentParser('argument for training')
+    
+    parser.add_argument('data', metavar='DIR',
+                        help='path to dataset')
+    parser.add_argument('--batch_size', type=int, default=128, help='batch size')
+    parser.add_argument('--num_workers', type=int, default=8, help='num of workers to use')
+    parser.add_argument('--cost', type=str, default='0.1,0.5,1')
+    parser.add_argument('--seed', default=0, type=int)
+    
+    # model definition
+    parser.add_argument('-a', '--arch', metavar='ARCH', default='resnet50',
+                        choices=model_names,
+                        help='model architecture: ' +
+                            ' | '.join(model_names) +
+                            ' (default: resnet50)')    
+    parser.add_argument('--pretrained', default='', type=str,
+                        help='path to pretrained checkpoint')
+    # dataset
+    parser.add_argument('--low_shot', default=False, action='store_true', help='whether to perform low-shot training.')    
+    
+    opt = parser.parse_args()
+
+    opt.num_class = 20
+    
+    # if low shot experiment, do 5 random runs
+    if opt.low_shot:
+        opt.n_run = 5
+    else:
+        opt.n_run = 1
+    return opt
+
+
+def calculate_ap(rec, prec):
+    """
+    Computes the AP under the precision recall curve.
+    """
+    rec, prec = rec.reshape(rec.size, 1), prec.reshape(prec.size, 1)
+    z, o = np.zeros((1, 1)), np.ones((1, 1))
+    mrec, mpre = np.vstack((z, rec, o)), np.vstack((z, prec, z))
+    for i in range(len(mpre) - 2, -1, -1):
+        mpre[i] = max(mpre[i], mpre[i + 1])
+
+    indices = np.where(mrec[1:] != mrec[0:-1])[0] + 1
+    ap = 0
+    for i in indices:
+        ap = ap + (mrec[i] - mrec[i - 1]) * mpre[i]
+    return ap
+
+def get_precision_recall(targets, preds):
+    """
+    [P, R, score, ap] = get_precision_recall(targets, preds)
+    Input    :
+        targets  : number of occurrences of this class in the ith image
+        preds    : score for this image
+    Output   :
+        P, R   : precision and recall
+        score  : score which corresponds to the particular precision and recall
+        ap     : average precision
+    """
+    # binarize targets
+    targets = np.array(targets > 0, dtype=np.float32)
+    tog = np.hstack((
+        targets[:, np.newaxis].astype(np.float64),
+        preds[:, np.newaxis].astype(np.float64)
+    ))
+    ind = np.argsort(preds)
+    ind = ind[::-1]
+    score = np.array([tog[i, 1] for i in ind])
+    sortcounts = np.array([tog[i, 0] for i in ind])
+
+    tp = sortcounts
+    fp = sortcounts.copy()
+    for i in range(sortcounts.shape[0]):
+        if sortcounts[i] >= 1:
+            fp[i] = 0.
+        elif sortcounts[i] < 1:
+            fp[i] = 1.
+    P = np.cumsum(tp) / (np.cumsum(tp) + np.cumsum(fp))
+    numinst = np.sum(targets)
+    R = np.cumsum(tp) / numinst
+    ap = calculate_ap(R, P)
+    return P, R, score, ap
+
+
+def main():
+    args = parse_option()
+    
+    random.seed(args.seed)
+    np.random.seed(args.seed)
+       
+    mean = [0.485, 0.456, 0.406]
+    std = [0.229, 0.224, 0.225]
+    normalize = transforms.Normalize(mean=mean, std=std)        
+    transform = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),        
+        transforms.ToTensor(),
+        normalize,
+    ])
+
+    train_dataset = Voc2007Classification(args.data,set='trainval',transform = transform)
+    val_dataset = Voc2007Classification(args.data,set='test',transform = transform)
+
+    val_loader = torch.utils.data.DataLoader(
+        val_dataset, batch_size=args.batch_size, shuffle=False,
+        num_workers=args.num_workers, pin_memory=True)
+
+    # create model
+    print("=> creating model '{}'".format(args.arch))
+    model = models.__dict__[args.arch](num_classes=128)
+
+    # load from pre-trained
+    if args.pretrained:
+        if os.path.isfile(args.pretrained):
+            print("=> loading checkpoint '{}'".format(args.pretrained))
+            checkpoint = torch.load(args.pretrained, map_location="cpu")            
+            state_dict = checkpoint['state_dict']
+            # rename pre-trained keys
+            for k in list(state_dict.keys()):
+                if k.startswith('module.encoder_q') and not k.startswith('module.encoder_q.fc'):
+                    # remove prefix
+                    state_dict[k[len("module.encoder_q."):]] = state_dict[k]
+                # delete renamed or unused k
+                del state_dict[k]  
+            model.load_state_dict(state_dict, strict=False)
+            model.fc = torch.nn.Identity()
+            print("=> loaded pre-trained model '{}'".format(args.pretrained))
+        else:
+            print("=> no checkpoint found at '{}'".format(args.pretrained))
+            
+    model.cuda()   
+    model.eval()   
+
+    test_feats = []
+    test_labels = []    
+    print('==> calculate test features')
+    for idx, (images, target) in enumerate(val_loader):
+        images = images.cuda(non_blocking=True)
+        feat = model(images)
+        feat = feat.detach().cpu()
+        test_feats.append(feat)
+        test_labels.append(target)
+
+    test_feats = torch.cat(test_feats,0).numpy()
+    test_labels = torch.cat(test_labels,0).numpy()
+               
+    test_feats_norm = np.linalg.norm(test_feats, axis=1)
+    test_feats = test_feats / (test_feats_norm + 1e-5)[:, np.newaxis]
+    
+    result={}
+    
+    if args.low_shot:
+        k_list = [1,2,4,8,16] #number of samples per-class for low-shot classifcation
+    else:
+        k_list = ['full']
+        
+    for k in k_list:
+        cost_list = args.cost.split(',')
+        result_k = np.zeros(len(cost_list))
+        for i,cost in enumerate(cost_list):
+            cost = float(cost)
+            avg_map = []
+            for run in range(args.n_run):
+                if args.low_shot: # sample k-shot training data
+                    print('==> re-sampling training data')
+                    train_dataset.convert_low_shot(k)    
+                print(len(train_dataset))
+
+                train_loader = torch.utils.data.DataLoader(
+                    train_dataset, batch_size=args.batch_size, shuffle=False,
+                    num_workers=args.num_workers, pin_memory=True)
+
+                train_feats = []
+                train_labels = []
+                print('==> calculate train features')
+                for idx, (images, target) in enumerate(train_loader):
+                    images = images.cuda(non_blocking=True)
+                    feat = model(images)
+                    feat = feat.detach()  
+
+                    train_feats.append(feat)                
+                    train_labels.append(target)
+
+                train_feats = torch.cat(train_feats,0).cpu().numpy()
+                train_labels = torch.cat(train_labels,0).cpu().numpy()
+
+                train_feats_norm = np.linalg.norm(train_feats, axis=1)
+                train_feats = train_feats / (train_feats_norm + 1e-5)[:, np.newaxis]
+
+                print('==> training SVM Classifier')
+                cls_ap = np.zeros((args.num_class, 1))
+                test_labels[test_labels==0] = -1 
+                train_labels[train_labels==0] = -1 
+                for cls in range(args.num_class):
+                    clf = LinearSVC(
+                        C=cost, class_weight={1: 2, -1: 1}, intercept_scaling=1.0,
+                        penalty='l2', loss='squared_hinge', tol=1e-4,
+                        dual=True, max_iter=2000, random_state=0)
+                    clf.fit(train_feats, train_labels[:,cls])
+
+                    prediction = clf.decision_function(test_feats)                                      
+                    P, R, score, ap = get_precision_recall(test_labels[:,cls], prediction)
+                    cls_ap[cls][0] = ap*100
+                mean_ap = np.mean(cls_ap, axis=0)
+
+                print('==> Run%d mAP is %.2f: '%(run,mean_ap))
+                avg_map.append(mean_ap)
+
+            avg_map = np.asarray(avg_map)     
+            print('Cost:%.2f - Average ap is: %.2f' %(cost,avg_map.mean()))
+            print('Cost:%.2f - Std is: %.2f' %(cost,avg_map.std()))
+            result_k[i]=avg_map.mean()
+        result[k] = result_k.max()    
+    print(result)    
+    
+if __name__ == '__main__':
+    main()
+
---
+++ pcl/builder.py
@@ -0,0 +1,214 @@
+import torch
+import torch.nn as nn
+from random import sample
+
+class MoCo(nn.Module):
+    """
+    Build a MoCo model with: a query encoder, a key encoder, and a queue
+    https://arxiv.org/abs/1911.05722
+    """
+    def __init__(self, base_encoder, dim=128, r=16384, m=0.999, T=0.1, mlp=False):
+        """
+        dim: feature dimension (default: 128)
+        r: queue size; number of negative samples/prototypes (default: 16384)
+        m: momentum for updating key encoder (default: 0.999)
+        T: softmax temperature 
+        mlp: whether to use mlp projection
+        """
+        super(MoCo, self).__init__()
+
+        self.r = r
+        self.m = m
+        self.T = T
+
+        # create the encoders
+        # num_classes is the output fc dimension
+        self.encoder_q = base_encoder(num_classes=dim)
+        self.encoder_k = base_encoder(num_classes=dim)
+
+        if mlp:  # hack: brute-force replacement
+            dim_mlp = self.encoder_q.fc.weight.shape[1]
+            self.encoder_q.fc = nn.Sequential(nn.Linear(dim_mlp, dim_mlp), nn.ReLU(), self.encoder_q.fc)
+            self.encoder_k.fc = nn.Sequential(nn.Linear(dim_mlp, dim_mlp), nn.ReLU(), self.encoder_k.fc)
+
+        for param_q, param_k in zip(self.encoder_q.parameters(), self.encoder_k.parameters()):
+            param_k.data.copy_(param_q.data)  # initialize
+            param_k.requires_grad = False  # not update by gradient
+
+        # create the queue
+        self.register_buffer("queue", torch.randn(dim, r))
+        self.queue = nn.functional.normalize(self.queue, dim=0)
+
+        self.register_buffer("queue_ptr", torch.zeros(1, dtype=torch.long))
+
+    @torch.no_grad()
+    def _momentum_update_key_encoder(self):
+        """
+        Momentum update of the key encoder
+        """
+        for param_q, param_k in zip(self.encoder_q.parameters(), self.encoder_k.parameters()):
+            param_k.data = param_k.data * self.m + param_q.data * (1. - self.m)
+
+    @torch.no_grad()
+    def _dequeue_and_enqueue(self, keys):
+        # gather keys before updating queue
+        keys = concat_all_gather(keys)
+
+        batch_size = keys.shape[0]
+
+        ptr = int(self.queue_ptr)
+        assert self.r % batch_size == 0  # for simplicity
+
+        # replace the keys at ptr (dequeue and enqueue)
+        self.queue[:, ptr:ptr + batch_size] = keys.T
+        ptr = (ptr + batch_size) % self.r  # move pointer
+
+        self.queue_ptr[0] = ptr
+
+    @torch.no_grad()
+    def _batch_shuffle_ddp(self, x):
+        """
+        Batch shuffle, for making use of BatchNorm.
+        *** Only support DistributedDataParallel (DDP) model. ***
+        """
+        # gather from all gpus
+        batch_size_this = x.shape[0]
+        x_gather = concat_all_gather(x)
+        batch_size_all = x_gather.shape[0]
+
+        num_gpus = batch_size_all // batch_size_this
+
+        # random shuffle index
+        idx_shuffle = torch.randperm(batch_size_all).cuda()
+
+        # broadcast to all gpus
+        torch.distributed.broadcast(idx_shuffle, src=0)
+
+        # index for restoring
+        idx_unshuffle = torch.argsort(idx_shuffle)
+
+        # shuffled index for this gpu
+        gpu_idx = torch.distributed.get_rank()
+        idx_this = idx_shuffle.view(num_gpus, -1)[gpu_idx]
+
+        return x_gather[idx_this], idx_unshuffle
+
+    @torch.no_grad()
+    def _batch_unshuffle_ddp(self, x, idx_unshuffle):
+        """
+        Undo batch shuffle.
+        *** Only support DistributedDataParallel (DDP) model. ***
+        """
+        # gather from all gpus
+        batch_size_this = x.shape[0]
+        x_gather = concat_all_gather(x)
+        batch_size_all = x_gather.shape[0]
+
+        num_gpus = batch_size_all // batch_size_this
+
+        # restored index for this gpu
+        gpu_idx = torch.distributed.get_rank()
+        idx_this = idx_unshuffle.view(num_gpus, -1)[gpu_idx]
+
+        return x_gather[idx_this]
+
+    def forward(self, im_q, im_k=None, is_eval=False, cluster_result=None, index=None):
+        """
+        Input:
+            im_q: a batch of query images
+            im_k: a batch of key images
+            is_eval: return momentum embeddings (used for clustering)
+            cluster_result: cluster assignments, centroids, and density
+            index: indices for training samples
+        Output:
+            logits, targets, proto_logits, proto_targets
+        """
+        
+        if is_eval:
+            k = self.encoder_k(im_q)  
+            k = nn.functional.normalize(k, dim=1)            
+            return k
+        
+        # compute key features
+        with torch.no_grad():  # no gradient to keys
+            self._momentum_update_key_encoder()  # update the key encoder
+
+            # shuffle for making use of BN
+            im_k, idx_unshuffle = self._batch_shuffle_ddp(im_k)
+
+            k = self.encoder_k(im_k)  # keys: NxC
+            k = nn.functional.normalize(k, dim=1)
+
+            # undo shuffle
+            k = self._batch_unshuffle_ddp(k, idx_unshuffle)
+
+        # compute query features
+        q = self.encoder_q(im_q)  # queries: NxC
+        q = nn.functional.normalize(q, dim=1)
+        
+        # compute logits
+        # Einstein sum is more intuitive
+        # positive logits: Nx1
+        l_pos = torch.einsum('nc,nc->n', [q, k]).unsqueeze(-1)
+        # negative logits: Nxr
+        l_neg = torch.einsum('nc,ck->nk', [q, self.queue.clone().detach()])
+
+        # logits: Nx(1+r)
+        logits = torch.cat([l_pos, l_neg], dim=1)
+
+        # apply temperature
+        logits /= self.T
+
+        # labels: positive key indicators
+        labels = torch.zeros(logits.shape[0], dtype=torch.long).cuda()
+
+        # dequeue and enqueue
+        self._dequeue_and_enqueue(k)
+        
+        # prototypical contrast
+        if cluster_result is not None:  
+            proto_labels = []
+            proto_logits = []
+            for n, (im2cluster,prototypes,density) in enumerate(zip(cluster_result['im2cluster'],cluster_result['centroids'],cluster_result['density'])):
+                # get positive prototypes
+                pos_proto_id = im2cluster[index]
+                pos_prototypes = prototypes[pos_proto_id]    
+                
+                # sample negative prototypes
+                all_proto_id = [i for i in range(im2cluster.max())]       
+                neg_proto_id = set(all_proto_id)-set(pos_proto_id.tolist())
+                neg_proto_id = sample(neg_proto_id,self.r) #sample r negative prototypes 
+                neg_prototypes = prototypes[neg_proto_id]    
+
+                proto_selected = torch.cat([pos_prototypes,neg_prototypes],dim=0)
+                
+                # compute prototypical logits
+                logits_proto = torch.mm(q,proto_selected.t())
+                
+                # targets for prototype assignment
+                labels_proto = torch.linspace(0, q.size(0)-1, steps=q.size(0)).long().cuda()
+                
+                # scaling temperatures for the selected prototypes
+                temp_proto = density[torch.cat([pos_proto_id,torch.LongTensor(neg_proto_id).cuda()],dim=0)]  
+                logits_proto /= temp_proto
+                
+                proto_labels.append(labels_proto)
+                proto_logits.append(logits_proto)
+            return logits, labels, proto_logits, proto_labels
+        else:
+            return logits, labels, None, None
+
+
+# utils
+@torch.no_grad()
+def concat_all_gather(tensor):
+    """
+    Performs all_gather operation on the provided tensors.
+    *** Warning ***: torch.distributed.all_gather has no gradient.
+    """
+    tensors_gather = [torch.ones_like(tensor)
+        for _ in range(torch.distributed.get_world_size())]
+    torch.distributed.all_gather(tensors_gather, tensor, async_op=False)
+
+    output = torch.cat(tensors_gather, dim=0)
+    return output
