964da1fb7c0546e8ce55627fa3c0debde4b7e456
Junnan Li <junnan.li@salesforce.com>
2022-05-27 18:28:39 +0800

    Update builder.py

--- pcl/builder.py
+++ pcl/builder.py
@@ -175,7 +175,7 @@ class MoCo(nn.Module):
                 pos_prototypes = prototypes[pos_proto_id]    
                 
                 # sample negative prototypes
-                all_proto_id = [i for i in range(im2cluster.max())]       
+                all_proto_id = [i for i in range(im2cluster.max()+1)]       
                 neg_proto_id = set(all_proto_id)-set(pos_proto_id.tolist())
                 neg_proto_id = sample(neg_proto_id,self.r) #sample r negative prototypes 
                 neg_prototypes = prototypes[neg_proto_id]    
